3cebcb146258dd815b60f9f694d880fd3206bcb0
Buck Doyle <buck.doyle@gmail.com>
2013-11-30 11:52:30 -0500

    Partially correct extraction of assessment data using filesystem cache rather than DBM

--- export-evalweb.rb
+++ export-evalweb.rb
@@ -3,56 +3,39 @@
 # Ubuntu: sudo apt-get install rubygem libxslt1-dev && sudo gem install mechanize
 require 'rubygems'
 require 'mechanize'
-require 'dbm'
 
-columns = [
-  {'id' => :i},
-  {'adresse' => :s}, {'ville' => :s}, {'proprietaire' => :s},
-  {'arrondissement' => :s}, {'arrondissement_no' => :i},
-  {'matricule' => :s}, {'compte' => :i}, {'uef_id' => :i},
-  {'cubf' => :i}, {'nb_logements' => :i}, {'voisinage' => :i},
-  {'emplacement_front' => :i}, {'emplacemment_profondeur' => :i}, {'emplacement_superficie' => :i},
-  {'classe_non_residentielle' => :i}, {'classe_industrielle' => :s}, {'terrain_vague' => :s},
-  {'annee_construction' => :i}, {'nb_etages' => :i}, {'nb_autres_locaux' => :i},
-  {'annee_role_anterieur' => :i}, {'terrain_role_anterieur' => :i},
-  {'batiment_role_anterieur' => :i}, {'immeuble_role_anterieur' => :i}, {'vide' => :s},
-  {'loi' => :s}, {'article' => :s}, {'alinea' => :s}, {'valeur_terrain' => :i},
-  {'valeur_batiment' => :i}, {'valeur_immeuble' => :i},
-  {'code_imposition' => :s}, {'code_exemption' => :s}, {'maj' => :s},
-  {'no_lot_renove' => :i}, {'paroisse' => :s}, {'lot' => :s}, {'subd' => :s},
-  {'type_lot' => :s}, {'superficie' => :f}, {'front' => :f}, {'profond' => :f}
-]
+columns = []
 
-File.open("evaluations.sql", 'w') do |sql|
-  sql.write("create database evalmtl ENGINE=InnoDB;\n")
-  sql.write("use evalmtl\n")
-  sql_types = {:s => "varchar(255)", :i => "integer", :f => "float"}
-  sql.write("create table evaluations (\n" + columns.map{|c| c.map{|col,t| "  #{col} #{sql_types[t]}"}}.join(",\n") + "\n) ENGINE=InnoDB;\n")
-  sql.write("LOAD DATA LOCAL INFILE 'evaluations.csv' INTO TABLE evaluations CHARACTER SET UTF8 IGNORE 1 LINES;\n")
-  sql.write("CREATE INDEX adresse_index ON evaluations (adresse);\n")
-  sql.write("CREATE INDEX proprietaire_index ON evaluations (proprietaire);\n")
-  sql.write("CREATE INDEX arrondissement_index ON evaluations (arrondissement);\n")
-  sql.write("CREATE INDEX arrondissement_no_index ON evaluations (arrondissement_no);\n")
-  sql.write("CREATE INDEX type_lot_index ON evaluations (type_lot);\n")
-  sql.write("CREATE INDEX uef_id_index ON evaluations (uef_id);\n")
-end
+columns[14] = {label: 'adresse', type: :string}
+columns[26] = {label: 'utilisation', type: :string}
+columns[39] = {label: 'proprietaire', type: :string}
+columns[145] = {label: 'valeur_anterieur', type: :integer}
+columns[154] = {label: 'valeur', type: :integer}
 
-db = DBM.open('address')
-File.open("evaluations.csv", 'w:UTF-8') do |csv|
-  csv.write(columns.map{|c|c.keys}.join("\t"))
+cached = Dir.glob 'cache/address/*'
+
+File.open("evaluations.tsv", 'w:UTF-8') do |csv|
+  csv.write((['evalweb_id'] + columns.compact.map{|c| c[:label]}).join("\t"))
   csv.write("\n")
-  db.each_entry do |address_id, page_content|
+  cached.each do |filename|
+    address_id = filename.split("/").last
+    page_content = File.read(filename)
+    puts "Working on address with ID #{address_id}"
+
     page_content.force_encoding('utf-8')
     page = Nokogiri::HTML::Document.parse(page_content, encoding='UTF-8')
     data = page.css("//td").map {|td| td.content.gsub(/\s+/, " ").strip}
     data = data.each_with_index.map { |cell,index|
-      type = columns[index].values if columns[index]
-      type == :s ? cell : cell.gsub(',','')
-    }
+      if columns[index]
+        puts "  (#{columns[index][:label]} #{cell})"
+        type = columns[index][:type]
+        type == :string ? cell : cell.gsub(',','')
+      else
+        nil
+      end
+    }.compact
     data.unshift(address_id)
     csv.write(data.join("\t"))
     csv.write("\n")
   end
 end
-db.close
-
