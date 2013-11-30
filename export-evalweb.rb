d5ec4affd99520d39db7c49735d8dd9d150ac063
Buck Doyle <buck.doyle@gmail.com>
2013-11-30 12:16:02 -0500

    Use XPath to extract assessment data to handle variable number of owners

--- export-evalweb.rb
+++ export-evalweb.rb
@@ -4,37 +4,30 @@
 require 'rubygems'
 require 'mechanize'
 
-columns = []
-
-columns[14] = {label: 'adresse', type: :string}
-columns[26] = {label: 'utilisation', type: :string}
-columns[39] = {label: 'proprietaire', type: :string}
-columns[145] = {label: 'valeur_anterieur', type: :integer}
-columns[154] = {label: 'valeur', type: :integer}
-
 cached = Dir.glob 'cache/address/*'
+columns = ['address_id', 'address', 'usage', 'owners', 'value', 'previous_value']
 
 File.open("evaluations.tsv", 'w:UTF-8') do |csv|
-  csv.write((['evalweb_id'] + columns.compact.map{|c| c[:label]}).join("\t"))
+  csv.write(columns.join("\t"))
   csv.write("\n")
-  cached.each do |filename|
+  cached.each_with_index do |filename, index|
     address_id = filename.split("/").last
     page_content = File.read(filename)
-    puts "Working on address with ID #{address_id}"
+    puts "Working on address with ID #{address_id} #{index} of #{cached.length}"
 
     page_content.force_encoding('utf-8')
     page = Nokogiri::HTML::Document.parse(page_content, encoding='UTF-8')
-    data = page.css("//td").map {|td| td.content.gsub(/\s+/, " ").strip}
-    data = data.each_with_index.map { |cell,index|
-      if columns[index]
-        puts "  (#{columns[index][:label]} #{cell})"
-        type = columns[index][:type]
-        type == :string ? cell : cell.gsub(',','')
-      else
-        nil
-      end
-    }.compact
-    data.unshift(address_id)
+
+    address = page.search("//tr[td/font[contains(., 'Adresse :')]]/td[2]//font").text.strip
+    usage = page.search("//tr[td/font[contains(., 'Utilisation prédominante :')]]/td[2]//font").text.strip
+
+    value = page.search("//tr[td/font[contains(., 'immeuble :')]]/td[2]//font").text.strip.gsub(" ", "").to_i
+    previous_value = page.search("//tr[td/font[contains(., 'immeuble au')]]/td[2]//font").text.strip.gsub(" ", "").to_i
+
+    owners = page.search("//tr[td/font[contains(., 'Nom :')]]/td[2]//font").map(&:text).join(" | ")
+
+    data = [address_id, address, usage, owners, value, previous_value]
+
     csv.write(data.join("\t"))
     csv.write("\n")
   end
