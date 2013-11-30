c4a4be6fe1e31938b1493446e7aec961cd379b76
Buck Doyle <buck.doyle@gmail.com>
2013-11-30 11:10:45 -0500

    Introduce hack to store addresses and streets in filesystem rather than DBM to avoid segfaults

--- lib/cache.rb
+++ lib/cache.rb
@@ -2,7 +2,15 @@ require 'dbm'
 
 class ScrapeCache
   def initialize
-    @databases = Hash.new { |dbs, namespace| dbs[namespace] = DBM.open(namespace) }
+    @databases = Hash.new do |dbs, namespace|
+      if namespace == "address"
+        FileCache.new(namespace)
+      elsif namespace == "street"
+        SlashConvertingFileCache.new(namespace)
+      else
+        DBM.open(namespace)
+      end
+    end
   end
   def put(namespace, key, value)
     if value.nil?
@@ -31,3 +39,45 @@ class ScrapeCache
   end
 end
 
+class FileCache
+  def initialize(namespace)
+    @namespace = namespace
+    FileUtils.mkdir_p(@namespace) unless File.directory?(@namespace)
+  end
+
+  def [](key)
+    if include?(key)
+      File.read(path(key))
+    else
+      nil
+    end
+  end
+
+  def []=(key, value)
+    File.open(path(key), 'w') {|f| f.write(value) }
+  end
+
+  def include?(key)
+    File.exists?(path(key))
+  end
+
+  def has_key?(key)
+    include?(key)
+  end
+
+  protected
+  def filename(key)
+    key
+  end
+
+  def path(key)
+    "cache/#{@namespace}/#{filename(key)}"
+  end
+end
+
+class SlashConvertingFileCache < FileCache
+  protected
+  def filename(key)
+    key.gsub("/", "_")
+  end
+end
