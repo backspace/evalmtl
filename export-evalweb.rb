0e6a1e24aa18f22244a16fb450db80cf975b7abc
Buck Doyle <b@chromatin.ca>
2014-11-27 00:53:30 -0500

    Restore TSV generation

--- export-evalweb.rb
+++ export-evalweb.rb
@@ -13,28 +13,39 @@ DataMapper.auto_migrate!
 cached = Dir.glob 'cache/address/*'
 columns = ['address_id', 'address', 'usage', 'owners', 'value', 'previous_value']
 
-cached.each_with_index do |filename, index|
-  address_id = filename.split("/").last
-  page_content = File.read(filename)
-  puts "Working on address with ID #{address_id} #{index} of #{cached.length}"
+File.open("evaluations.tsv", 'w:UTF-8') do |csv|
+  csv.write(columns.join("\t"))
+  csv.write("\n")
 
-  page_content.force_encoding('utf-8')
-  page = Nokogiri::HTML::Document.parse(page_content, encoding='UTF-8')
+  cached.each_with_index do |filename, index|
+    address_id = filename.split("/").last
+    page_content = File.read(filename)
+    puts "Working on address with ID #{address_id} #{index} of #{cached.length}"
 
-  address = page.search("//tr[td/font[contains(., 'Adresse :')]]/td[2]//font").text.strip
-  usage = page.search("//tr[td/font[contains(., 'Utilisation prédominante :')]]/td[2]//font").text.strip
+    page_content.force_encoding('utf-8')
+    page = Nokogiri::HTML::Document.parse(page_content, encoding='UTF-8')
 
-  value = page.search("//tr[td/font[contains(., 'immeuble :')]]/td[2]//font").text.strip.gsub(" ", "").to_i
-  previous_value = page.search("//tr[td/font[contains(., 'immeuble au')]]/td[5]//font").text.strip.gsub(" ", "").to_i
+    address = page.search("//tr[td/font[contains(., 'Adresse :')]]/td[2]//font").text.strip
+    usage = page.search("//tr[td/font[contains(., 'Utilisation prédominante :')]]/td[2]//font").text.strip
 
-  owners = page.search("//tr[td/font[contains(., 'Nom :')]]/td[2]//font").map(&:text)
+    value = page.search("//tr[td/font[contains(., 'immeuble :')]]/td[2]//font").text.strip.gsub(" ", "").to_i
+    previous_value = page.search("//tr[td/font[contains(., 'immeuble au')]]/td[5]//font").text.strip.gsub(" ", "").to_i
 
-  unit = Unit.create(
-    evalweb_id: address_id,
-    address: address,
-    usage: Usage.first_or_create(name: usage),
-    owners: owners.map{|owner_string| Owner.first_or_create(name: owner_string)},
-    value: value,
-    previous_value: previous_value
-  )
+    owners = page.search("//tr[td/font[contains(., 'Nom :')]]/td[2]//font").map(&:text)
+
+    unit = Unit.create(
+      evalweb_id: address_id,
+      address: address,
+      usage: Usage.first_or_create(name: usage),
+      owners: owners.map{|owner_string| Owner.first_or_create(name: owner_string)},
+      value: value,
+      previous_value: previous_value
+    )
+
+
+    data = [address_id, address, usage, owners, value, previous_value]
+
+    csv.write(data.join("\t"))
+    csv.write("\n")
+  end
 end
